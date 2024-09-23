cf77ed2895450b48acd9983916b8b963ebf7d1ce
Nikita Gritsenko <nik.gretsenko1@gmail.com>
2024-09-23 19:18:37 +0700

    Initial commit

---
+++ app/src/main/java/com/example/toasthandler/MainActivity.kt
@@ -0,0 +1,29 @@
+package com.example.toasthandler
+
+import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        // Создание объекта button и установка обработчика нажатия
+        val button: Button = findViewById(R.id.button_ok)
+        button.setOnClickListener {
+            Toast.makeText(this, "Кнопка ОК", Toast.LENGTH_SHORT).show()
+        }
+    }
+}
